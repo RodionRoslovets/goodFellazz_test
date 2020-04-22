822f42981734c3bee96323dceb551a61d0742422
Rodion <rodion18570@gmail.com>
2020-04-22 11:41:19 +0300

    created theme

---
+++ theme_goodFellazz/footer.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package goofFellazz
+ */
+
+?>
+
+<footer>
+	<div class="container d-sm-flex justify-content-between">
+		<p>2019 (с) Все права защищены. БухОне.ру</p>
+		<p>Разработано: BuhOne</p>
+	</div>
+</footer>
+
+
+<?php wp_footer(); ?>
+</body>
+
+</html>
